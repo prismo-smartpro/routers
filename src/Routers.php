01668a387fdcf6dac89c83a4caba89c8af1ccd74
Prismo Smart PRO <prismo.smartpro@gmail.com>
2024-10-01 06:32:15 -0300

    Support for middlewares

--- src/Routers.php
+++ src/Routers.php
@@ -3,6 +3,8 @@
 namespace SmartPRO\Technology;
 
 
+use Exception;
+
 class Routers
 {
     /** @var array */
@@ -17,8 +19,9 @@ class Routers
     private string $group;
     /** @var int|null */
     private ?int $error = null;
-
+    protected ?string $errorString = null;
     protected ?array $route;
+    protected ?array $middleware = null;
 
 
     const NOT_FOUND = 404;
@@ -33,131 +36,175 @@ class Routers
     /**
      * @param $name
      * @param $handler
+     * @param array $middleware
      * @return void
      */
-    public function delete($name, $handler)
+    public function delete($name, $handler, array $middleware = array()): void
     {
-        $this->addRouter("DELETE", $name, $handler);
+        $this->addRouter("DELETE", $name, $handler, $middleware);
     }
 
     /**
      * @param $name
      * @param $handler
+     * @param array $middleware
      * @return void
      */
-    public function put($name, $handler)
+    public function put($name, $handler, array $middleware = array()): void
     {
-        $this->addRouter("PUT", $name, $handler);
+        $this->addRouter("PUT", $name, $handler, $middleware);
     }
 
     /**
      * @param $name
      * @param $handler
+     * @param array $middleware
      * @return void
      */
-    public function get($name, $handler)
+    public function get($name, $handler, array $middleware = array()): void
     {
-        $this->addRouter("GET", $name, $handler);
+        $this->addRouter("GET", $name, $handler, $middleware);
     }
 
     /**
      * @param $name
      * @param $handler
+     * @param array $middleware
      * @return void
      */
-    public function post($name, $handler)
+    public function post($name, $handler, array $middleware = array()): void
     {
-        $this->addRouter("POST", $name, $handler);
+        $this->addRouter("POST", $name, $handler, $middleware);
     }
 
     /**
      * @param $method
      * @param $name
      * @param $handler
+     * @param array $middleware
      * @return void
      */
-    private function addRouter($method, $name, $handler)
+    private function addRouter($method, $name, $handler, array $middleware = array()): void
     {
         $name = empty($this->group) ? $name : "/{$this->group}{$name}";
-        if ($name != "/") {
-            $name = rtrim($name, "/");
-        }
+        $name = rtrim($name, "/") ?: "/";
+
         preg_match_all('~{(.*?)}~', $name, $data);
         $replaceName = preg_replace('~{(.*?)}~', '([^/]+)', $name);
         $replaceName = "/^" . str_replace('/', '\/', $replaceName) . "$/";
-        if (!is_callable($handler)) {
-            $handler = explode(":", $handler);
-            $Controller = "\\" . $handler[0] ?? null;
-            $Action = $handler[1] ?? null;
-        } else {
+
+        if (is_callable($handler)) {
             $isCallable = $handler;
+            $Controller = $Action = null;
+        } else {
+            [$Controller, $Action] = explode(":", $handler) + [null, null];
+            $Controller = "\\" . $Controller;
+            $isCallable = null;
         }
+
         $this->routers[$method][$replaceName] = [
             "router" => $name,
             "namespace" => $this->namespace,
             "method" => $method,
-            "handler" => $Controller ?? null,
-            "action" => $Action ?? null,
+            "handler" => $Controller,
+            "action" => $Action,
             "data" => $data[1],
-            "isCallable" => $isCallable ?? null
+            "isCallable" => $isCallable,
+            "middleware" => empty($middleware) ? $this->middleware : $middleware
         ];
     }
 
-    /**
-     * @return void
-     */
-    public function dispatch()
+
+    public function dispatch(): void
     {
-        if (!empty($this->routers[$this->httpMethod])) {
+        try {
+            $this->uri = rtrim($this->uri, "/") ?: "/";
+            if (empty($this->routers[$this->httpMethod])) {
+                throw new Exception("Routers does not exist", 404);
+            }
+
             foreach ($this->routers[$this->httpMethod] as $key => $value) {
-                if($this->uri != "/"){
-                   $this->uri = rtrim($this->uri, "/");
-                }
-                preg_match_all($key, $this->uri, $results, PREG_SET_ORDER);
-                if (!empty($results)) {
+                if (preg_match_all($key, $this->uri, $results, PREG_SET_ORDER)) {
                     unset($results[0][0]);
+
                     $this->route = [
                         "results" => $results,
                         "values" => $value
                     ];
+                    break;
                 }
             }
 
-            if (!empty($this->route['values'])) {
-                if (!empty($this->route['values']['isCallable'])
-                    and is_callable($this->route['values']['isCallable'])) {
-                    call_user_func($this->route['values']['isCallable'], [], $this);
-                } else {
-                    $Action = $this->route['values']['action'] ?? null;
-                    $data = array_combine($this->route['values']['data'], $this->route['results'][0]);
-                    $className = $this->route['values']['namespace'] . $this->route['values']['handler'];
-                    if (class_exists($className)) {
-                        $Controller = new $className();
-                        if (method_exists($Controller, $Action)) {
-                            $Controller->$Action($data ?? []);
-                        } else {
-                            $this->error = self::NOT_IMPLEMENTED;
-                        }
-                    } else {
-                        $this->error = self::NOT_FOUND;
-                    }
-                }
-            } else {
-                $this->error = self::NOT_FOUND;
+            if (empty($this->route['values'])) {
+                throw new Exception("Router does not exist", 404);
             }
-        } else {
-            $this->error = self::NOT_FOUND;
+
+            $middlewares = $this->route["values"]["middleware"];
+            foreach ($middlewares as $middleware) {
+                $middlewareClass = $middleware[0] ?? null;
+                $middlewareMethod = $middleware[1] ?? null;
+
+                if (empty($middlewareClass) or empty($middlewareMethod)) {
+                    throw new Exception("Middleware data is invalid", 400);
+                }
+
+                if (!class_exists($middlewareClass)) {
+                    throw new Exception("Middleware does not exist", 404);
+                }
+
+                if (!method_exists($middlewareClass, $middlewareMethod)) {
+                    throw new Exception("Middleware not implemented", 405);
+                }
+
+                $carryMiddleware = new $middlewareClass();
+                $executeMiddleware = $carryMiddleware->$middlewareMethod();
+                if ($executeMiddleware === false) {
+                    throw new Exception("Unsatisfactory request", 400);
+                }
+            }
+
+            if (!empty($this->route['values']['isCallable']) && is_callable($this->route['values']['isCallable'])) {
+                call_user_func($this->route['values']['isCallable'], [], $this);
+                return;
+            }
+
+            $Action = $this->route['values']['action'] ?? null;
+            $data = array_combine($this->route['values']['data'] ?? [], $this->route['results'][0] ?? []);
+            $className = $this->route['values']['namespace'] . $this->route['values']['handler'];
+
+            if (!class_exists($className)) {
+                throw new Exception("Handle not implemented", 404);
+            }
+
+            $Controller = new $className();
+            if (!method_exists($Controller, $Action)) {
+                throw new Exception("Method does not exist", 405);
+            }
+
+            $Controller->$Action($data);
+
+        } catch (Exception $exception) {
+            $this->errorString = $exception->getMessage();
+            $this->error = $exception->getCode();
         }
     }
 
     /**
      * @param string $group
-     * @return void
+     * @return Routers
      */
     public function group(string $group): Routers
     {
         $this->group = $group;
         return $this;
+    }
+
+    /**
+     * @param array|null $middleware
+     */
+    public function middleware(?array $middleware): void
+    {
+        $this->middleware[] = $middleware;
     }
 
     /**
@@ -175,5 +222,18 @@ class Routers
     public function error(): ?int
     {
         return $this->error;
+    }
+
+    public function resetMiddlewares(): void
+    {
+        $this->middleware = [];
+    }
+
+    /**
+     * @return string|null
+     */
+    public function errorString(): ?string
+    {
+        return $this->errorString;
     }
 }
